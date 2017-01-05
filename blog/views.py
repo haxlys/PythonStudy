c95e388ab35766c796636e4d672db7f3f3983e1f
haxlys <haxlys@gmail.com>
2017-01-05 13:28:29 +0900

    수정 및 상세화면 등록

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 # Create your views here.
 def post_list(requeset):
@@ -11,3 +11,30 @@ def post_list(requeset):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST": # 서브밋할 경우 method가 POST다.
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False) #commit=False로 설정한 이유 : author, published_date 값을 사용자화면에서 입력벋지 않았기 때문에 넣어주고 commit 하겠다는 뜻.
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else: #새로 등록하는 화면으로 가는 경우
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
