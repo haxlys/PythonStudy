8ce371cdf6d29588419c149bfbfb2d33083009bd
haxlys <haxlys@gmail.com>
2017-01-05 14:31:50 +0900

    로그인 로그아웃 구현

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+
 from .models import Post
 from .forms import PostForm
 
